c15c4e748345b561ec5135e094e7bdcf51cfc63a
mancuoj <1337381768@qq.com>
2024-01-18 21:40:11 +0800

    style: css reset in 2024

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app.tsx'
+import './styles/reset.css'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
