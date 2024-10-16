be4cf016e725fef5b72daf1b83a25dc31a71fb2b
mancuoj <mancuoj@gmail.com>
2024-10-16 18:32:25 +0800

    chore: update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import '@fontsource-variable/inter'
 import '@fontsource-variable/source-serif-4'
 import '@fontsource-variable/jetbrains-mono'
 import { Home } from './components/home.tsx'
-import './style.css'
+import './globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
