2526fd7c5b21d4fc785fb0252607aac47109bc35
mancuoj <mancuoj@gmail.com>
2024-10-26 15:38:32 +0800

    chore: update

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,7 +4,7 @@ export function Home() {
   return (
     <div className="font-sans antialiased">
       <div className="flex min-h-dvh flex-col items-center justify-center gap-4 dark:bg-gray-900 dark:text-white">
-        <CodeBlock code="npx degit mancuoj-collective/react-mini-tmpl app" />
+        <CodeBlock code="npx degit mancuoj-collective/react-tmpl-lite app" />
         <div className="flex items-center gap-2 text-xl">
           <a href="https://github.com/mancuoj" className="i-lucide-github" />
           <a href="https://mancuoj.me" className="i-lucide-smile" />
