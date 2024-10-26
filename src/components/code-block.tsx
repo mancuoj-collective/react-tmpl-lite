3d1b626a0ef56d29d05205c713f71e795b4e7249
mancuoj <mancuoj@gmail.com>
2024-10-26 12:35:29 +0800

    chore: update

--- src/components/code-block.tsx
+++ src/components/code-block.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react'
-import { cn } from '../lib/utils'
 
 export function CodeBlock({ code }: { code: string }) {
   const [hasCopied, setHasCopied] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       setHasCopied(false)
     }, 2000)
+
+    return () => clearTimeout(timeout)
   }, [hasCopied])
 
   const handleCopy = () => {
@@ -16,13 +17,13 @@ export function CodeBlock({ code }: { code: string }) {
   }
 
   return (
-    <div className="flex items-center gap-3 rounded-xl bg-gray-100 p-3 px-5 dark:bg-gray-800 dark:text-white">
+    <div className="flex items-center gap-3 rounded-xl bg-gray-100 px-4 py-3 dark:bg-gray-800 dark:text-white">
       <div className="font-mono">{code}</div>
-      <button
-        title="Copy Code"
-        onClick={handleCopy}
-        className={cn(`${hasCopied ? 'i-lucide-clipboard-check' : 'i-lucide-clipboard'}`)}
-      />
+      {hasCopied ? (
+        <button className="i-lucide-clipboard-check" />
+      ) : (
+        <button title="Copy Code" onClick={handleCopy} className="i-lucide-clipboard" />
+      )}
     </div>
   )
 }
