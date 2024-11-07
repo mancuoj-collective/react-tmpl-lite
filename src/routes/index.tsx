9d01096e7270bf7743c46cf50b60c97478f30404
mancuoj <mancuoj@gmail.com>
2024-11-07 23:43:48 +0800

    chore: update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,13 @@ import { Button } from '@/components/ui/button'
 import { Layout } from './layout'
 import { CopyInput } from '@/components/copy-input'
 import { RiArticleLine, RiChatSmileAiLine, RiGithubLine } from '@remixicon/react'
+import { DarkModeToggle } from '@/components/dark-mode-toggle'
 
 export function App() {
   return (
     <Layout>
       <div className="flex min-h-svh flex-col items-center justify-center gap-5">
-        <CopyInput className="w-[30rem]" defaultValue="npx degit mancuoj-collective/react-tmpl-lite app" />
+        <CopyInput className="w-[20rem] sm:w-[30rem]" defaultValue="npx degit mancuoj-collective/react-tmpl-lite app" />
         <div className="flex gap-1.5">
           <Button variant="outline" size="icon" asChild>
             <a href="https://github.com/mancuoj-collective/react-tmpl-lite" target="_blank" rel="noreferrer">
@@ -24,6 +25,7 @@ export function App() {
               <RiChatSmileAiLine />
             </a>
           </Button>
+          <DarkModeToggle />
         </div>
       </div>
     </Layout>
