01957c8c9210602cfcef14f1a3215c3a1c0d95c8
mancuoj <mancuoj@gmail.com>
2024-12-10 19:22:01 +0800

    chore: update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,21 @@
 import { Button } from '@/components/ui/button'
 import { DarkModeToggle } from '@/components/dark-mode-toggle'
+import { TwScreenIndicator } from '@/components/tw-screen-indicator'
 import { AppProvider } from './provider'
 
 export function App() {
   return (
     <AppProvider>
-      <div className="flex min-h-svh items-center justify-center gap-2">
-        <Button variant="outline" size="icon" asChild className="rounded-full">
-          <a href="https://github.com/mancuoj-collective/react-tmpl-lite">
-            <span className="i-carbon-logo-github" />
-          </a>
-        </Button>
-        <DarkModeToggle className="rounded-full" />
+      <div className="flex min-h-svh flex-col items-center justify-center gap-5">
+        <div className="space-x-2">
+          <Button variant="outline" size="icon" asChild className="rounded-full">
+            <a href="https://github.com/mancuoj-collective/react-tmpl-lite">
+              <span className="i-carbon-logo-github" />
+            </a>
+          </Button>
+          <DarkModeToggle className="rounded-full" />
+        </div>
+        <TwScreenIndicator />
       </div>
     </AppProvider>
   )
