09abe0685b0cff6d7f929fbbdd2c36ce29a3cc6a
mancuoj <mancuoj@gmail.com>
2025-04-14 01:23:37 +0800

    chore: re init

--- src/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button'
-
-export function App() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Hello World</Button>
-    </div>
-  )
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
-
 import { createRoot } from 'react-dom/client'
-import { App } from './app'
-import './globals.css'
+import './index.css'
+import { App } from './App'
 
-createRoot(document.querySelector('#root')!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
