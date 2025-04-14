887ae7e0193fe7e690f24eed6a7455aa05818e75
mancuoj <mancuoj@gmail.com>
2025-04-14 23:09:52 +0800

    feat: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button'
+import { ThemeToggle } from '@/components/theme-toggle'
 
 export function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh font-dm-sans antialiased">
-      <Button>Click me</Button>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <ThemeToggle />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App'
-import '@fontsource-variable/dm-sans'
+import { ThemeProvider } from './components/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
