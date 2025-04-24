12d7967d9e210c73c3f1f80ce2f43f742cf1ac4e
mancuoj <mancuoj@gmail.com>
2025-04-24 19:06:40 +0800

    chore: update

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
-import { Providers } from '@/providers'
-
 import { App } from '@/routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from 'next-themes'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Navigate, Route, Routes } from 'react-router'
-import './index.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import '@/index.css'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Providers>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Providers>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider storageKey="react-tmpl-lite-theme" attribute="class">
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<App />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
 )
