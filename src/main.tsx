05e53319101d6bd6097ce9f50abecd6d00f94c74
mancuoj <mancuoj@gmail.com>
2025-05-26 18:56:51 +0800

    chore: update

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,19 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ThemeProvider } from 'next-themes'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
-import { App } from '@/pages'
-import '@/styles/globals.css'
+import './globals.css'
 
-const queryClient = new QueryClient()
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <button className="btn btn-primary">
+        React
+      </button>
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider storageKey="react-tmpl-lite-theme" attribute="class">
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<App />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
