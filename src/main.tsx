4dfd8c97579065845347521fde8f7abd2fc67927
mancuoj <mancuoj@gmail.com>
2025-07-24 22:58:23 +0800

    feat: react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HeroUIProvider } from '@heroui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { App } from './app'
 import './globals.css'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HeroUIProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HeroUIProvider>
   </StrictMode>,
 )
